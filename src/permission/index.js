1f45649c4b646673d01c56aa6e28d35c825c6e6d
lancer2672 <jbkhanhtran@gmail.com>
2024-04-02 15:00:18 +0700

    add noti permission

---
+++ src/permission/index.js
@@ -0,0 +1,17 @@
+import {PermissionsAndroid} from 'react-native';
+
+export const requestNotificationPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.NOTIFICATIONS,
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.error('Error requesting notification permission:', error);
+    return false;
+  }
+};
