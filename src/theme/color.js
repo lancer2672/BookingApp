876e6a9861967898641f6e0a3a89a8e612bd05d8
lancer2672 <jbkhanhtran@gmail.com>
2024-04-02 14:54:41 +0700

    setup theme color

---
+++ src/theme/color.js
@@ -0,0 +1,42 @@
+export const generalColor = {
+  white: {
+    100: '#fff',
+  },
+  black: {
+    100: '#000',
+  },
+  other: {
+    yellow: '#e1fa25',
+    bluepurple: '#514eb6',
+  },
+};
+
+export const whiteThemeColors = {
+  ...generalColor,
+  bg: {
+    primary: '#f6f9ff',
+    secondary: '#3498db',
+    tertiary: '#3c50c7',
+  },
+  text: {
+    primary: '#000000',
+    secondary: '#5068f2',
+    error: '#b85454',
+    success: '#138000',
+  },
+};
+
+export const darkThemeColors = {
+  ...generalColor,
+  bg: {
+    primary: '#000',
+    secondary: '#1c1c1c',
+    tertiary: '#3c50c7',
+  },
+  text: {
+    primary: '#fff',
+    secondary: '#5068f2',
+    error: '#b85454',
+    success: '#138000',
+  },
+};
