1a6843716a61b422f48b931d0f53fbbb9e70eb67
ndpmmo <85108119+tuananh382@users.noreply.github.com>
2024-04-11 00:45:05 +0700

    HomeScreen update

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import AgentDashboard from '@src/screens/AgentScreens/Dashboard';
 import ThemeProviderComponent from '@src/theme/context';
 import {SafeAreaView} from 'react-native';
 import FlashMessage from 'react-native-flash-message';
+import HomeScreen from '@src/screens/HomeScreen/HomeScreen';
 
 function App() {
   return (
     <ThemeProviderComponent>
       <SafeAreaView style={{flex: 1}}>
-        <AgentDashboard></AgentDashboard>
+        {/* <AgentDashboard></AgentDashboard> */}
+          <HomeScreen/>
         <FlashMessage position="top" />
       </SafeAreaView>
     </ThemeProviderComponent>
--- src/screens/HomeScreen/Component/HomeHeader.js
+++ src/screens/HomeScreen/Component/HomeHeader.js
@@ -1,14 +1,57 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import ButtonComponent from '@src/components/Button';
+
 
 const HomeHeader = () => {
   return (
-    <View>
-      <Text>HomeHeader</Text>
+    <View style={styles.homeHeader}>
+      <ButtonComponent style={styles.button}>
+        <Text style={styles.buttonText}>X</Text>
+      </ButtonComponent>
+      <ButtonComponent style={styles.button}>
+        <Text style={styles.buttonText}>X</Text>
+      </ButtonComponent>
+      <ButtonComponent style={styles.button}>
+        <Text style={styles.buttonText}>X</Text>
+      </ButtonComponent>
+      <ButtonComponent style={styles.button}>
+        <Text style={styles.buttonText}>X</Text>
+      </ButtonComponent>
     </View>
   );
 };
 
-export default HomeHeader;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    width: "100%",
+  },
+  homeHeader: {
+    backgroundColor: 'white',
+    padding: 10,
+    display: "flex",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    alignSelf: 'center',
+    position: "absolute",
+    bottom:0,
+    borderColor: "black",
+    borderTopWidth: 1,
+  },
+  button: {
+    borderRadius: 5,
+    padding: 5,
+    width:80,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
 
-const styles = StyleSheet.create({});
+export default HomeHeader;
